c1857a48d0d81c422fb59acd72007f3d9cd611e8
qaq112233 <84634293+qaq112233@users.noreply.github.com>
2023-04-13 22:37:24 +0800

    Add files via upload

---
+++ 第八周/13.py
@@ -0,0 +1,7 @@
+price = 100
+jinjia = 50
+profit = price - jinjia
+maoli = profit / price
+lirunlv = profit / jinjia
+
+print(profit, lirunlv, maoli)
---
+++ 第八周/16.py
@@ -0,0 +1,6 @@
+a = input("一个句子")
+b = input("一个单词")
+c = input("另一个单词")
+
+d = a.replace(b, c)
+print(d)
