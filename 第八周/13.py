e5d9be98ccb0d8b9653dbfc2fa7c6132fc67dbd5
qaq112233 <84634293+qaq112233@users.noreply.github.com>
2023-06-11 01:15:58 +0800

    Delete 第八周 directory

--- 第八周/13.py
+++
@@ -1,7 +0,0 @@
-price = 100
-jinjia = 50
-profit = price - jinjia
-maoli = profit / price
-lirunlv = profit / jinjia
-
-print(profit, lirunlv, maoli)
--- 第八周/19.py
+++
@@ -1,4 +0,0 @@
-a = eval(input('薪水'))
-b = a*1.05**3
-c = a*1.15
-print(b, c)
